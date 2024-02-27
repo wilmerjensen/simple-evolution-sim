59302a6bbfcac1d8734a2b5b726abaa5bf24345b
Wilmer Jensen <wilmer.sternehall@gmail.com>
2024-02-27 13:15:36 +0100

    added generations, kill all creatures in kill zones at the end of every generation

--- src/main.py
+++ src/main.py
@@ -12,8 +12,10 @@ BLOCK_SIZE = 12
 POPULATION = 200
 SYNAPSES = 8
 
-TICKS_PER_SECOND = 30
+TICKS_PER_SECOND = 60
 TICKS_PER_GENERATION = 300
+GENERATION_TICK_COUNT = 0
+GENERATION_COUNT = 0
 
 white = (255, 255, 255)
 green = (0, 255, 0)
@@ -33,8 +35,10 @@ def main():
     game_window.grid = environment.Grid(game_window, game_window.grid_size_x, game_window.grid_size_x, game_window.block_size)
     game_window.grid.add_kill_zone((0,0), ((GRID_SIZE_X * BLOCK_SIZE) / 10, GRID_SIZE_Y * BLOCK_SIZE))
 
-
     creature_list = creatures.Creature.generate_creatures(game_window.grid, POPULATION, SYNAPSES)
+
+    generation_tick_count = 0
+    generation_count = 0
 
     while running:
 
@@ -55,9 +59,28 @@ def main():
         pygame.display.update()
         clock.tick(TICKS_PER_SECOND)
 
+        generation_tick_count += 1
+        if generation_tick_count == TICKS_PER_GENERATION:
+            print("New Generation!")
+            generation_tick_count = 0
+            generation_count += 1
+            kill_creatures_in_kill_zones(game_window.grid, creature_list)
+            game_window.draw()
+            pygame.display.update()
+            clock.tick(TICKS_PER_SECOND)
+            pygame.time.wait(2000)
+
     pygame.quit()
 
 
+def kill_creatures_in_kill_zones(grid: environment.Grid, creature_list: list):
+    for c in creature_list:
+        c: creatures.Creature
+        for kz in grid.kill_zones:
+            if c.block.rect.colliderect(kz):
+                c.block.remove_creature()
+                creature_list.remove(c)
+                del c
 
 if __name__ == "__main__":
     main()
